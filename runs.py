19bb1fc68d6ee08853b2067f14fa68181cb37a60
Nitesh kumar Gupta <guptanitesh2711@gmail.com>
2022-09-22 10:46:34 +0530

    cimmiitng complete

--- runs.py
+++ runs.py
@@ -1,9 +1,12 @@
 import os
+import numpy as np
 
-p1s = range(0,5)
-p2s = range(0,5)
+p1s = np.linspace(0,1,0.5)
+p2s = np.linspace(0,1,0.5)
 
-for p1 in p1s:
-    for p2 in p2s:
-        print(f"logging experiment for p1:{p1}, p2: {p2}")
-        os.system(f"python demo.py -p1 {p1} -p2 {p2}")
+if __name__=='__main__':
+
+    for p1 in p1s:
+        for p2 in p2s:
+            print(f"logging experiment for p1:{p1}, p2: {p2}")
+            os.system(f"python demo.py -a {p1} -l1 {p2}")
